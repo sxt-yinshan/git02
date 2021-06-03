bd54255b532e89c1dc3e8ded196559b89fd0cbe1
yinshan <970814398@qq.com>
2021-06-03 20:57:19 +0800

    第一次提交 dev冲突

--- java/com/text/test.java
+++ java/com/text/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test.main");
         System.out.println("test.main");
+        System.out.println(232);
     }
 }
